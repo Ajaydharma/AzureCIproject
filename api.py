8386f82690f21ef92be3420bc1be846fb76d6b51
ajaydharma <ajaykannan532000@gmail.com>
2023-05-11 18:15:47 +0100

    first commit

---
+++ api.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# API 1: Hello World
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
+
+# API 2: Square a number
+@app.route('/square/<int:num>')
+def square(num):
+    result = num * num
+    return jsonify({'input': num, 'result': result})
+
+if __name__ == '__main__':
+    app.run(debug=True)
